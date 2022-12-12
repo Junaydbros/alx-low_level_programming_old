3f627ed2ca3d00a5d85e7128b762cef7d2fa9c31
Abdulhamid <hamidejisanusi@gmail.com>
2022-12-12 13:13:50 -0800

    Attempt 01

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 1
+ */
+int main(void)
+{
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n", str);
+	return (1);
+}

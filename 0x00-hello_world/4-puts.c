604f1cb24aa8ef01397aacfa6e4e9721f6f85f0a
Abdulhamid <hamidejisanusi@gmail.com>
2022-12-12 04:31:06 -0800

    Attempt 02

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/* main - Entry
+ * Return - Always 0
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
 

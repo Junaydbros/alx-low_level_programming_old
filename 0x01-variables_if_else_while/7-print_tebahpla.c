0b6a0307aab5c7df2e228bb859fa1b0ab58827d3
Abdulhamid <hamidejisanusi@gmail.com>
2022-12-14 23:27:04 -0800

    Attempt 01

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}

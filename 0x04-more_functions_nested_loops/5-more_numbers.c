98e276097365bfa39a7baa42b45ce35e050c15ae
Abdulhamid <hamidejisanusi@gmail.com>
2022-12-18 06:08:18 -0800

    Attempt Play

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,14 @@ void more_numbers(void)
 {
 	int n;
 
-	for (n = 1; n <= 10; n++)
+	for (n = 0; n <= 10; n++)
 	{
 	int ch;
 
 	for (ch = 0; ch <= 14; ch++)
 	{
 		if (ch >= 10)
-		_putchar('1');
+		_putchar('0');
 		_putchar(ch % 10 + '0');
 	}
 	_putchar('\n');

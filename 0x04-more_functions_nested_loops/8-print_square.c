6497731fce3d4a00ff9bfc4dc314c9cdd71f822d
Abdulhamid <hamidejisanusi@gmail.com>
2022-12-19 01:30:29 -0800

    Attempt 03

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,14 +6,12 @@
  */
 void print_square(int size)
 {
+	int a, b;
+
 	if (size <= 0)
 		_putchar('\n');
-	int a;
-
 	for (a = 0; a < (size); a++)
 	{
-		int b;
-
 		for (b = 0; b < (size); b++)
 		{
 			_putchar('#');

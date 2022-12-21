48c9ff3fab3e8eca8457a23227364d48b62c6e70
Abdulhamid <hamidejisanusi@gmail.com>
2022-12-20 23:11:45 -0800

    Attempt 01

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - file that tests the function positive_or_negative()
+ * Return: correct output
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+	positive_or_negative(a);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This directory contains projects that tests the knowledge of debugging

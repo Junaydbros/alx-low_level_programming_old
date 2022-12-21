54fc28d9d898831e85e427826c4c4d191856ec8f
Abdulhamid <hamidejisanusi@gmail.com>
2022-12-20 23:17:43 -0800

    Attempt 02

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * main - file that tests the function positive_or_negative()
- * Return: correct output
+ * Return: Always 0
  */
 
 int main(void)
 {
-	int a;
+	int i;
 
-	a = 0;
-	positive_or_negative(a);
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }

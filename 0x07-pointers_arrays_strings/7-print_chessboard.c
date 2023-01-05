19f46b13f74be42633255160939f388486a63bb2
Abdulhamid <hamidejisanusi@gmail.com>
2023-01-05 08:19:06 +0000

    Attempt 02

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_chessboard - function that prints the chessboard
+ * @a: character
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	for (int c = 0; c < 8; c++)
+	{
+		for (int d = 0; d < 8; d++)
+		{
+			if ((c + d) % 2 == 0)
+			{
+				a[c][d] = 'B';
+			}
+			else
+			{
+				a[c][d] = 'W';
+			}
+			printf("%c ", a[c][d]);
+		}
+		printf("\n");
+	}
+}

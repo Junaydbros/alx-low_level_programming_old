220cf367cc8cf7e39471078d767319f09c31651e
Abdulhamid <hamidejisanusi@gmail.com>
2023-01-14 12:48:36 +0000

    Attempt 01

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the number of arguments passed into it
+ * the program prints a number, followed by a new line
+ * @argc: the argument count
+ * @argv: the argument
+ * Return: void
+ */
+
+int main(int argc, char 88argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
